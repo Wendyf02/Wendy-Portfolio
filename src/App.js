4a1c587db794ab68f13e8475a612e9c9ed5a6d7a
Wendy Fgigueroa <camomila444@gmail>
2021-05-29 17:35:35 -0400

    adding folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import NavBar from "./components/NavBar"
+import Home from "./components/Home"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <switch>
+        <Route exact path={["/"]}>
+            <Jumbotron />
+             <Home />
+        </Route>
+
+
+      </switch>
+   
     </div>
   );
 }
